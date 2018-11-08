ef7b2a15e06e836a929d13a63306e41102e021a0
Esau Mtz <mtzab6@gmail.com>
2018-11-08 10:41:33 -0600

    Migraciones y Clase Context

---
+++ Tiendita-Moreno/Tiendita-Moreno.BussinessEntities/Migrations/201811081618201_Initial.cs
@@ -0,0 +1,93 @@
+namespace Tiendita_Moreno.BussinessEntities.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cut",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        InitialAmount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        FinalAmount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Deference = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Date = c.DateTime(nullable: false, precision: 0),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Department",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50, unicode: false, storeType: "nvarchar"),
+                        Description = c.String(nullable: false, maxLength: 100, unicode: false, storeType: "nvarchar"),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Product",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Description = c.String(nullable: false, maxLength: 50, unicode: false, storeType: "nvarchar"),
+                        BarCode = c.String(nullable: false, maxLength: 16, unicode: false, storeType: "nvarchar"),
+                        DepartmentId = c.Int(nullable: false),
+                        Cost = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Ganancia = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Current_Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Department", t => t.DepartmentId, cascadeDelete: true)
+                .Index(t => t.DepartmentId);
+            
+            CreateTable(
+                "dbo.DetailSale",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        SaleId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Amount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Product", t => t.ProductId, cascadeDelete: true)
+                .ForeignKey("dbo.Sale", t => t.SaleId, cascadeDelete: true)
+                .Index(t => t.ProductId)
+                .Index(t => t.SaleId);
+            
+            CreateTable(
+                "dbo.Sale",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        DateOfSale = c.DateTime(nullable: false, precision: 0),
+                        Amount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        QuantityOfItem = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.DetailSale", "SaleId", "dbo.Sale");
+            DropForeignKey("dbo.DetailSale", "ProductId", "dbo.Product");
+            DropForeignKey("dbo.Product", "DepartmentId", "dbo.Department");
+            DropIndex("dbo.DetailSale", new[] { "SaleId" });
+            DropIndex("dbo.DetailSale", new[] { "ProductId" });
+            DropIndex("dbo.Product", new[] { "DepartmentId" });
+            DropTable("dbo.Sale");
+            DropTable("dbo.DetailSale");
+            DropTable("dbo.Product");
+            DropTable("dbo.Department");
+            DropTable("dbo.Cut");
+        }
+    }
+}
---
+++ Tiendita-Moreno/Tiendita-Moreno.BussinessEntities/StoreDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiendita_Moreno.BussinessEntities
+{
+
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class StoreDbContext : DbContext
+    {
+
+        public StoreDbContext() : base(StoreDbContext.getConnectionStringName())
+        {   }
+
+        public DbSet<Cut> Cuts { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DetailSale> DetailSales { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public static string getConnectionStringName()
+        {
+
+            ConnectionStringSettingsCollection connections = ConfigurationManager.ConnectionStrings;
+            String CONNECTIONSTRING = String.Empty;
+
+            if (connections.Count != 0)
+            {
+                foreach (ConnectionStringSettings connection in connections)
+                {
+                    CONNECTIONSTRING = connection.Name;
+                }
+            }
+
+            return CONNECTIONSTRING;
+
+        }
+
+    }
+}
